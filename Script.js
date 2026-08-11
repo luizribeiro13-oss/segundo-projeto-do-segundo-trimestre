ff41658b087202eecda92aecbb4ccb2ebc29d433
luizribeiro13-oss <luiz.ribeiro13@escola.pr.gov.br>
2026-08-10 22:50:35 -0300

    segundo projeto

--- Script.js
+++ Script.js
@@ -3,4 +3,14 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-const perguntas = ["Pergunta 1", "Pergunta 2"];
+const perguntas = ["O faturamneto esta positivo ?", "Como melhorar ?"];
+const perguntas = [
+  {
+    enunciado: "O faturamneto esta positivo ?",
+    alternativas: ["Sim", "Não"],
+  },
+  {
+    enunciado: "Como melhorar ?",
+    alternativas: ["Aumentando as vendas", "Melhorando o Gerenciamento"],
+  },
+];
