8fc311dc9b86f0106c8de6292690ac3d3078eb14
luizribeiro13-oss <luiz.ribeiro13@escola.pr.gov.br>
2026-08-17 22:44:37 -0300

    futuro da ia

--- Script.js
+++ Script.js
@@ -1,16 +1,9 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
-const perguntas = ["O faturamneto esta positivo ?", "Como melhorar ?"];
+const caixaPrincipal document.querySelector(".caixa-principal");
+const caixaPerguntas document.querySelector(".caixa-perguntas");
+const caixaAlternativas document.querySelector(".caixa-alternativas");
+const caixaResultado document.querySelector(".caixa-resultado");
+const textoResultado document.querySelector(".texto-resultado");
+
 const perguntas = [
-  {
-    enunciado: "O faturamneto esta positivo ?",
-    alternativas: ["Sim", "Não"],
-  },
-  {
-    enunciado: "Como melhorar ?",
-    alternativas: ["Aumentando as vendas", "Melhorando o Gerenciamento"],
-  },
-];
+    
+]
