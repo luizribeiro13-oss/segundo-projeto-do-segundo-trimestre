75cda8449b5929b061d025e7f0881a3f266d0c5e
luizribeiro13-oss <luiz.ribeiro13@escola.pr.gov.br>
2026-08-17 22:55:33 -0300

    projeto ia

--- Script.js
+++ Script.js
@@ -3,7 +3,15 @@ const caixaPerguntas document.querySelector(".caixa-perguntas");
 const caixaAlternativas document.querySelector(".caixa-alternativas");
 const caixaResultado document.querySelector(".caixa-resultado");
 const textoResultado document.querySelector(".texto-resultado");
-
-const perguntas = [
-    
+const perguntas [
+enunciado: "Pergunta 1",
+alternativas: [
+"Alternativa 1",
+"Aternativa 2"
 ]
+enunciado: "Pergunta 2",
+alternativas: [
+"Alternativa 1",
+"Aternativa 2"
+]
+J;
